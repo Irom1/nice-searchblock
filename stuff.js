0d8eae08221f49e7f0afbef5c80e1a1f5be9f3be
Liam <lvgallagher@gmail.com>
2019-06-12 15:52:22 -0400

    9.9

--- stuff.js
+++ stuff.js
@@ -1,10 +1,12 @@
 function myFunction() {
   document.getElementsByTagName("button")[0].setAttribute("class", "button");
+  document.getElementById("search").top = "24px";
   var x = document.getElementById("NAV");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   document.getElementsByTagName("button")[0].setAttribute("class", "democlass");
+  document.getElementById("search").top = "112px";
   }
 }
