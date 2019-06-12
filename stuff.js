474d17df24fe2bace8ba68c2a0adb88e795203d1
Liam <lvgallagher@gmail.com>
2019-06-12 15:37:54 -0400

    9.2

--- stuff.js
+++ stuff.js
@@ -1,12 +1,10 @@
 function myFunction() {
+  document.getElementsByTagName("button")[0].setAttribute("class", "democlass");
   var x = document.getElementById("NAV");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
+    document.getElementsByTagName("button")[0].setAttribute("button");
   }
-}
-
-function myFunction() {
-  document.getElementsByTagName("button")[0].setAttribute("class", "democlass");
 }
