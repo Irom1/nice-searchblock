51724755ba43930240471137e0ce74fb88b148c7
Liam <lvgallagher@gmail.com>
2019-06-12 16:03:48 -0400

    10.1

--- stuff.js
+++ stuff.js
@@ -1,12 +1,10 @@
 function myFunction() {
   document.getElementsByTagName("button")[0].setAttribute("class", "button");
-    document.getElementsById("search")[0].setAttribute("search2");
   var x = document.getElementById("NAV");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
     x.style.display = "none";
   document.getElementsByTagName("button")[0].setAttribute("class", "democlass");
-    document.getElementsById("search")[0].setAttribute("search");
   }
 }
