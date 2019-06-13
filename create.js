1aa5d1cb486f5dd577c36641422046ebae912c0f
Liam Vance Gallagher <Liam@Seans-MacBook-Pro.local>
2019-06-13 19:18:04 -0400

    6.0

--- create.js
+++
@@ -1,5 +0,0 @@
-object.onclick = function(){
-  chrome.bookmarks.create({'parentId': extensionsFolderId,
-                                 'title': 'Delete',
-                                 'url': 'javascript:location.replace("https://google.com");'});
-};
