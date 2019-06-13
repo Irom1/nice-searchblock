22dea209bab9352611434020b815c6aca6b99c20
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-13 11:43:52 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -8,8 +8,3 @@ function myFunction() {
   document.getElementsByTagName("button")[0].setAttribute("class", "democlass");
   }
 }
-
-$(function () {
-//replace() does not keep the originating page in the session history,
-document.location.replace("/Exercises#nocache"); // clear the last entry in the history and redirect to new url
-});
