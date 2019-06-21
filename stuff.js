a696936b94bc555150e8b24a4447a19d279e9f19
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 10:12:14 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -13,3 +13,16 @@ function functionbing() {
 function functionmusic() {
   document.getElementById("mainbox").src = "music";
 }
+
+$(document).ready(function() {
+  $("#bookmarkme").click(function() {
+    if (window.sidebar) { // Mozilla Firefox Bookmark
+      window.sidebar.addPanel(location.href,document.title,"");
+    } else if(window.external) { // IE Favorite
+      window.external.AddFavorite(location.href,document.title); }
+    else if(window.opera && window.print) { // Opera Hotlist
+      this.title=document.title;
+      return true;
+    }
+  });
+});
