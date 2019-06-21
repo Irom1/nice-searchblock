ce2dd5cf45f3c72602bc90f9e07f5c228e3af014
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 07:57:02 -0400

    Create stuff.js

---
+++ stuff.js
@@ -0,0 +1,15 @@
+function functionabout() {
+  document.getElementById("mainbox").src = "index";
+}
+
+function functionpgs() {
+  document.getElementById("mainbox").src = "pgs";
+}
+
+function functionbing() {
+  document.getElementById("mainbox").src = "bing";
+}
+
+function functionmusic() {
+  document.getElementById("mainbox").src = "music";
+}
