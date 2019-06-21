09a3d2e1af29a2ac0d1ca141091fea2a42292966
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 11:33:50 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -11,5 +11,5 @@ function functionbing() {
 }
 
 function functionmusic() {
-  document.getElementById("mainbox").src = "https://www.searchblock.cf/music";
+  document.getElementById("mainbox").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/62859766&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true";
 }
