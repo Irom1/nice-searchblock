024125984c242c131579bcca8473f680073f7b36
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 11:08:17 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,15 +1,15 @@
-function functionabout() {
+object.onclick = function functionabout() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/about";
 }
 
-function functionpgs() {
+object.onclick = function functionpgs() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/pgs";
 }
 
-function functionbing() {
+object.onclick = function functionbing() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/bing";
 }
 
-function functionmusic() {
+object.onclick = function functionmusic() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/music";
 }
