174b0010c4c08967a9b1e6cbbc5c633a1d1d2a77
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 22:40:44 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,24 +1,24 @@
 window.onload= function (){
-document.getElementById("about").addEventListener("click", myFunction); //adding click function to element
-function myFunction() {
+document.getElementById("about").addEventListener("click", functionabout); //adding click function to element
+function functionabout() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/about"; //changing mainbox's src
 }
 //end of change
 
- document.getElementById("pgs").addEventListener("click", myFunction); //adding click function to element
-function myFunction() {
+ document.getElementById("pgs").addEventListener("click", functionpgs); //adding click function to element
+function functionpgs() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/pgs"; //changing mainbox's src
 }
 //end of change
 
- document.getElementById("bing").addEventListener("click", myFunction); //adding click function to element
-function myFunction() {
+ document.getElementById("bing").addEventListener("click", functionbing); //adding click function to element
+function functionbing() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/bing"; //changing mainbox's src
 }
 //end of change
 
- document.getElementById("music").addEventListener("click", myFunction); //adding click function to element
-function myFunction() {
+ document.getElementById("music").addEventListener("click", functionmusic); //adding click function to element
+function functionmusic() {
   document.getElementById("mainbox").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/277152167&color=%23202b8f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"; //changing mainbox's src
 }
 //end of change
