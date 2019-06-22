7144a967f348ef3345faaf03c6a65a3e78f81f83
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 22:28:29 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,15 +1,23 @@
-function functionabout() {
-  document.getElementById("mainbox").src = "https://www.searchblock.cf/about";
+ document.getElementById("about").addEventListener("click", myFunction); //adding click function to element
+function myFunction() {
+  document.getElementById("mainbox").src = "https://www.searchblock.cf/about"; //changing mainbox's src
 }
+//end of change
 
-function functionpgs() {
-  document.getElementById("mainbox").src = "https://www.searchblock.cf/pgs";
+ document.getElementById("pgs").addEventListener("click", myFunction); //adding click function to element
+function myFunction() {
+  document.getElementById("mainbox").src = "https://www.searchblock.cf/pgs"; //changing mainbox's src
 }
+//end of change
 
-function functionbing() {
-  document.getElementById("mainbox").src = "https://www.searchblock.cf/bing";
+ document.getElementById("bing").addEventListener("click", myFunction); //adding click function to element
+function myFunction() {
+  document.getElementById("mainbox").src = "https://www.searchblock.cf/bing"; //changing mainbox's src
 }
+//end of change
 
-function functionmusic() {
-  document.getElementById("mainbox").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/62859766&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true";
+ document.getElementById("music").addEventListener("click", myFunction); //adding click function to element
+function myFunction() {
+  document.getElementById("mainbox").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/277152167&color=%23202b8f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"; //changing mainbox's src
 }
+//end of change
