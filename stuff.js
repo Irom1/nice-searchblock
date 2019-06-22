bf29612045473062d45dd1520cb868566f481674
Vanceagher <48972091+Vanceagher@users.noreply.github.com>
2019-06-21 22:37:20 -0400

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,4 +1,5 @@
- document.getElementById("about").addEventListener("click", myFunction); //adding click function to element
+window.onload= function (){
+document.getElementById("about").addEventListener("click", myFunction); //adding click function to element
 function myFunction() {
   document.getElementById("mainbox").src = "https://www.searchblock.cf/about"; //changing mainbox's src
 }
@@ -21,3 +22,4 @@ function myFunction() {
   document.getElementById("mainbox").src = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/277152167&color=%23202b8f&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"; //changing mainbox's src
 }
 //end of change
+}
